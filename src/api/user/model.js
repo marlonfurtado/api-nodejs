ef2f7cd821a1c232de62d48388c8aeef6bb37c87
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-12-23 20:30:46 -0200

    change user schema

--- src/api/user/model.js
+++ src/api/user/model.js
@@ -1,4 +1,3 @@
-import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 import randtoken from 'rand-token'
 import mongoose, { Schema } from 'mongoose'
@@ -8,22 +7,27 @@ import { env } from '../../config'
 const roles = ['user', 'admin']
 
 const userSchema = new Schema({
+  name: {
+    type: String,
+    index: true,
+    required: true,
+    trim: true
+  },
   email: {
     type: String,
     match: /^\S+@\S+\.\S+$/,
     required: true,
     unique: true,
-    trim: true,
-    lowercase: true
+    lowercase: true,
+    trim: true
   },
   password: {
     type: String,
     required: true,
     minlength: 6
   },
-  name: {
+  description: {
     type: String,
-    index: true,
     trim: true
   },
   services: {
@@ -33,26 +37,9 @@ const userSchema = new Schema({
     type: String,
     enum: roles,
     default: 'user'
-  },
-  picture: {
-    type: String,
-    trim: true
   }
 }, {
   timestamps: true
-})
-
-userSchema.path('email').set(function (email) {
-  if (!this.picture || this.picture.indexOf('https://gravatar.com') === 0) {
-    const hash = crypto.createHash('md5').update(email).digest('hex')
-    this.picture = `https://gravatar.com/avatar/${hash}?d=identicon`
-  }
-
-  if (!this.name) {
-    this.name = email.replace(/^(.+)@.+$/, '$1')
-  }
-
-  return email
 })
 
 userSchema.pre('save', function (next) {
@@ -70,10 +57,10 @@ userSchema.pre('save', function (next) {
 userSchema.methods = {
   view (full) {
     let view = {}
-    let fields = ['id', 'name', 'picture']
+    let fields = ['id', 'name', 'description']
 
     if (full) {
-      fields = [...fields, 'email', 'createdAt']
+      fields = [...fields, 'email', 'role', 'createdAt']
     }
 
     fields.forEach((field) => { view[field] = this[field] })
@@ -89,16 +76,16 @@ userSchema.methods = {
 userSchema.statics = {
   roles,
 
-  createFromService ({ service, id, email, name, picture }) {
+  createFromService ({ service, id, email, name, description }) {
     return this.findOne({ $or: [{ [`services.${service}`]: id }, { email }] }).then((user) => {
       if (user) {
         user.services[service] = id
         user.name = name
-        user.picture = picture
+        user.description = description
         return user.save()
       } else {
         const password = randtoken.generate(16)
-        return this.create({ services: { [service]: id }, email, password, name, picture })
+        return this.create({ services: { [service]: id }, email, password, name, description })
       }
     })
   }
